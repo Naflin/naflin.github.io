87b50bfe5f9534af8700143430eac6a7e24947f0
Naflin <nathanblue00@gmail.com>
2018-12-24 18:33:01 -0500

    added flex boxes

---
+++ index.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+
+    var boxes = document.getElementsByClassName("box");
+
+    var createNewBox = function() {
+        var newDiv = document.createElement('div');
+        newDiv.className = 'box';
+        newDiv.innerText = boxes.length+1;
+        newDiv.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+        return newDiv;
+    }
+
+    var myFunction = function() {
+        var parent = this.parentNode;
+        var newDiv = createNewBox();
+        
+        parent.appendChild(newDiv);
+        console.log("appended");
+        update();
+    };
+
+
+    var update = function() {
+        boxes = document.getElementsByClassName("box");
+        for (var i = 0; i < boxes.length; i++) {
+            boxes[i].addEventListener('click', myFunction, false);
+        }
+    }
+    
+    update();
+};
