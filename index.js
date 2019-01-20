9df6c2544ad33e911e91475daf563d6fd4d8b0f3
Naflin <nathanblue00@gmail.com>
2019-01-19 19:39:07 -0500

    updated base page

---
+++ index.js
@@ -0,0 +1,58 @@
+window.onload = function() {
+
+    var text = {};
+    text.opacityIn = [0,1];
+    text.scaleIn = [0.2, 1];
+    text.scaleOut = 3;
+    text.durationIn = 800;
+    text.durationOut = 600;
+    text.delay = 500;
+
+    anime.timeline({
+        loop: true
+    }).add({
+        targets: '.animated-text .letters-1',
+        opacity: text.opacityIn,
+        scale: text.scaleIn,
+        duration: text.durationIn
+    }).add({
+        targets: '.animated-text .letters-1',
+        opacity: 0,
+        scale: text.scaleOut,
+        duration: text.durationOut,
+        easing: "easeInExpo",
+        delay: text.delay
+    }).add({
+        targets: '.animated-text .letters-2',
+        opacity: text.opacityIn,
+        scale: text.scaleIn,
+        duration: text.durationIn
+    }).add({
+        targets: '.animated-text .letters-2',
+        opacity: 0,
+        scale: text.scaleOut,
+        duration: text.durationOut,
+        easing: "easeInExpo",
+        delay: text.delay
+    }).add({
+        targets: '.animated-text .letters-3',
+        opacity: text.opacityIn,
+        scale: text.scaleIn,
+        duration: text.durationIn
+    }).add({
+        targets: '.animated-text .letters-3',
+        opacity: 0,
+        scale: text.scaleOut,
+        duration: text.durationOut,
+        easing: "easeInExpo",
+        delay: text.delay
+    }).add({
+        targets: '.animated-text',
+        opacity: 0,
+        duration: 500,
+        delay: 500,
+        complete: function() {
+            window.location.href = '/home';
+        }
+    });
+};
